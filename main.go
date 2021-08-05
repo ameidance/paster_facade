1c3e79fa5c4053d804b3cbb7b5e2bf79991da851
Singularity0909 <904854724@qq.com>
2021-08-05 23:17:45 +0800

    refactor: redis & gin config

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/ameidance/paster_facade/client"
-	"github.com/ameidance/paster_facade/conf"
+	"github.com/bytedance/gopkg/util/logger"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 )
 
 var router *gin.Engine
@@ -13,7 +15,7 @@ var router *gin.Engine
 func main() {
 	client.InitRedis()
 
-	ginConf, err := conf.GetGinConfig()
+	ginConf, err := getGinConfig()
 	if err != nil {
 		panic(err)
 	}
@@ -21,4 +23,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type _GinConf struct {
+	Address string `yaml:"address"`
+	Port    int    `yaml:"port"`
+}
+
+const (
+	_GIN_CONF_PATH = "conf/gin.yml"
+)
+
+func getGinConfig() (*_GinConf, error) {
+	conf := new(_GinConf)
+	file, err := ioutil.ReadFile(_GIN_CONF_PATH)
+	if err != nil {
+		logger.Errorf("[getGinConfig] open file failed. err:%v", err)
+		return nil, err
+	}
+	if err = yaml.Unmarshal(file, conf); err != nil {
+		logger.Errorf("[getGinConfig] unmarshal file failed. err:%v", err)
+		return nil, err
+	}
+	return conf, nil
 }
