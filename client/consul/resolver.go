d614807339adda2ece81097c4793302572f98f43
Singularity0909 <904854724@qq.com>
2021-08-08 22:25:04 +0800

    feat: consul registery

---
+++ client/consul/resolver.go
@@ -0,0 +1,55 @@
+package consul
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cloudwego/kitex/pkg/discovery"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/hashicorp/consul/api"
+)
+
+type Resolver struct {
+}
+
+func NewResolver() *Resolver {
+	return &Resolver{}
+}
+
+func (*Resolver) Target(ctx context.Context, target rpcinfo.EndpointInfo) (description string) {
+	if Client == nil {
+		return
+	}
+
+	klog.Infof("[Resolver -> Target] service name:%v", target.ServiceName())
+	return target.ServiceName()
+}
+
+func (*Resolver) Resolve(ctx context.Context, desc string) (res discovery.Result, err error) {
+	if Client == nil {
+		return
+	}
+
+	var services []*api.ServiceEntry
+	services, _, err = Client.Health().Service(desc, "", true, nil)
+	if err != nil {
+		klog.Errorf("[Resolver -> Resolve] find healthy services failed. target service name:%v", desc)
+		return
+	}
+	for _, service := range services {
+		addr := fmt.Sprintf("%v:%v", service.Service.Address, service.Service.Port)
+		res.Instances = append(res.Instances, discovery.NewInstance("tcp", addr, 10, nil))
+	}
+	res.Cacheable = true
+	res.CacheKey = desc
+	return
+}
+
+func (*Resolver) Diff(cacheKey string, prev, next discovery.Result) (discovery.Change, bool) {
+	return discovery.DefaultDiff(cacheKey, prev, next)
+}
+
+func (*Resolver) Name() string {
+	return "ConsulResolver"
+}
--- client/rpc.go
+++ client/rpc.go
@@ -1,16 +1,17 @@
 package client
 
 import (
+	"github.com/ameidance/paster_facade/client/consul"
 	"github.com/ameidance/paster_facade/model/dto/kitex_gen/ameidance/paster/core/pastercoreservice"
 	"github.com/cloudwego/kitex/client"
 )
 
 var (
-	Resolver   *ConsulResolver
+	Resolver   *consul.Resolver
 	CoreClient pastercoreservice.Client
 )
 
 func InitRpc() {
-	Resolver = NewConsulResolver()
+	Resolver = consul.NewResolver()
 	CoreClient = pastercoreservice.MustNewClient("ameidance.paster.core", client.WithResolver(Resolver))
 }
